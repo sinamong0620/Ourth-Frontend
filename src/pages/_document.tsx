0a448a8557f1dfea34cb44b7d122e3f78bd26d7c
sinamong0620 <sinaskim60@gmail.com>
2023-03-08 23:51:42 +0900

    feat: input click & no scale

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, user-scalable=no"
+          content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width"
         />
       </Head>
       <body>
