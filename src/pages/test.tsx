ec90444875a8553c35c2cf92f0e8ed11e8b3c9e3
sinamong0620 <sinaskim60@gmail.com>
2023-03-13 02:37:08 +0900

    docs: video tag playsInline add

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -5,7 +5,7 @@ export default function Test() {
   return (
     <>
       <MainStyleContainer>
-        <video src="/videos/video.mp4" muted autoPlay loop />
+        <video src="/videos/video.mp4" muted autoPlay loop playsInline />
 
         <ScrollHeight>
           <MainTextContainer>
