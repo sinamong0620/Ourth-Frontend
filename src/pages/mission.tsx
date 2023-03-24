2fa9f5f04574edcb832725bd1815d7b3e567cdc3
sinamong0620 <sinaskim60@gmail.com>
2023-03-24 13:08:06 +0900

    docs  : mission img revise
    docs  : mission img revise

--- src/pages/mission.tsx
+++ src/pages/mission.tsx
@@ -2,42 +2,97 @@ import styled from "styled-components";
 import TodaysMission from "../components/mission/TodaysMission";
 import CompletedMission from "../components/mission/CompletedMission";
 import useMission from "../hooks/useMission";
-import Link from "next/link";
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import Layout from "../components/Layout";
+import axios from "axios";
+import Modal from "../components/mission/Modal";
 
 export default function Mission() {
-  const { userMission, saveRandomMission } = useMission();
+  const { userMission, saveRandomMission, missionArray } = useMission();
+  //모달 useState 설정
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const MissionCount = missionArray.filter((x) => x === true);
+  const imgArray = [
+    "/images/poo.png",
+    "/images/wink.png",
+    "/images/sunglasses.png",
+    "/images/hearteye.png",
+  ];
+  const [position, setPosition] = useState(0);
+  function onScroll() {
+    setPosition(window.scrollY);
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
+  const onClickAddBadge = async () => {
+    try {
+      const response = await axios.post(
+        "https://ourth.duckdns.org/badge/add",
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("tokenKey")}`,
+          },
+          withCredentials: true,
+        }
+      );
+      //missionArray의 값을 갱신해주기 위해서 한번 더 호출
+      saveRandomMission();
+    } catch (error) {
+      alert(error);
+    }
+  };
 
   return (
     <MainStyleContainer>
-      <div>
-        <h1>
-          이번 주 <b>미션</b>이에요
-        </h1>
-        {userMission.map((mission) =>
-          mission.status ? null : (
-            <div key={mission.id}>
-              <TodaysMission
-                id={mission.id}
-                todo={mission.text}
-                todomore="똥 좀 덜 쌉니다"
-                imgurl="/images/poo.png"
-                saveRandomMission={saveRandomMission}
-              />
-            </div>
-          )
-        )}
-        <h1>완료</h1>
+      {missionArray[0] ? (
+        <div>수고 했어요 모든 미션 완료하셨습니다</div>
+      ) : MissionCount.length === 4 ? (
+        <div>
+          뱃지 받게 하기
+          <button onClick={onClickAddBadge}>뱃지 받기</button>
+        </div>
+      ) : (
+        <div>
+          <h1>
+            이번 주 <b>미션</b>이에요
+          </h1>
+          {userMission.map((mission) =>
+            mission.status ? null : (
+              <div key={mission.id}>
+                <TodaysMission
+                  id={mission.id}
+                  todo={mission.text}
+                  imgurl={imgArray[mission.id - 1]}
+                  point={mission.point}
+                  saveRandomMission={saveRandomMission}
+                  setModalOpen={setModalOpen}
+                />
+              </div>
+            )
+          )}
+          <h1>완료</h1>
 
-        {userMission.map((mission) =>
-          mission.status ? (
-            <div key={mission.id}>
-              <CompletedMission todo={mission.text} imgurl="/images/poo.png" />
-            </div>
-          ) : null
-        )}
-      </div>
+          {userMission.map((mission) =>
+            mission.status ? (
+              <div key={mission.id}>
+                <CompletedMission
+                  todo={mission.text}
+                  point={mission.point}
+                  imgurl={imgArray[mission.id - 1]}
+                />
+              </div>
+            ) : null
+          )}
+        </div>
+      )}
+
+      {modalOpen && <Modal setModalOpen={setModalOpen} />}
     </MainStyleContainer>
   );
 }
