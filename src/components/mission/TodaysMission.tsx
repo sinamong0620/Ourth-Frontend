dedbe66b545ef9a7a6044d193e0aed88d8333f7e
sinamong0620 <sinaskim60@gmail.com>
2023-03-29 19:18:33 +0900

    feat : todaysMission modal func add

--- src/components/mission/TodaysMission.tsx
+++ src/components/mission/TodaysMission.tsx
@@ -1,55 +1,59 @@
 import styled from "styled-components";
 import Image from "next/image";
-import axios from "axios";
+import Modal from "./Modal";
+import { useState } from "react";
 
 interface IProps {
   id: number;
   todo: string;
-  todomore: string;
   imgurl: string;
-  saveRandomMission: () => {};
+  point: number;
+  onSuccess: () => Promise<void>;
 }
 
 const TodaysMission = (props: IProps) => {
-  const onCheckBoxChange = async (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    try {
-      const response = await axios.patch(
-        "https://ourth.duckdns.org/usermission/clear",
-        { id: (e.target as HTMLButtonElement).id }, //body는 두번째 인자로 넣어야함.
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("tokenKey")}`,
-          },
-        }
-      );
-      console.log(response);
-      props.saveRandomMission();
-    } catch (error) {
-      alert(error);
-    }
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+
+  const scrollStop = () => {
+    const body = document.querySelector("body") as HTMLElement;
+    const scrollPosition = window.pageYOffset;
+
+    body.style.overflow = "hidden";
+    //이거 추가하면 스크롤이 안되네 ..? 왤까나..
+    // body.style.pointerEvents = "none";
+    body.style.position = "fixed";
+    body.style.top = `-${scrollPosition}px`;
+    body.style.left = "0";
+    body.style.right = "0";
   };
+
   return (
-    <>
-      <TodayMissionComponent>
-        <div>
-          <Image src={props.imgurl} width={50} height={50} alt="체크" />
-          <TodaysMissionText>
-            <div>{props.todo}</div>
-            <div>{props.todomore}</div>
-          </TodaysMissionText>
-        </div>
-        <button
-          id={String(props.id)}
-          onClick={(e) => {
-            onCheckBoxChange(e);
-          }}
-        >
-          완료
-        </button>
-      </TodayMissionComponent>
-    </>
+    <TodayMissionComponent>
+      <div>
+        <Image src={props.imgurl} width={50} height={50} alt="체크" />
+        <TodaysMissionText>
+          <div>{props.todo}</div>
+          <div>{`${props.point}pt`}</div>
+        </TodaysMissionText>
+      </div>
+      <button
+        id={String(props.id)}
+        onClick={(e) => {
+          //모달 완성하고 나서 이거 옮겨주기
+          setModalOpen(true);
+          scrollStop();
+        }}
+      >
+        완료
+      </button>
+      {modalOpen && (
+        <Modal
+          setModalOpen={setModalOpen}
+          mId={props.id}
+          onClose={props.onSuccess}
+        />
+      )}
+    </TodayMissionComponent>
   );
 };
 export default TodaysMission;
@@ -93,3 +97,6 @@ const TodaysMissionText = styled.div`
     font-weight: 100;
   }
 `;
+function useRecoilState(textState: any): [any, any] {
+  throw new Error("Function not implemented.");
+}
